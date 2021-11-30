abac61f28d5948ec0b917ce3104d51ef26efa925
cainang <40045303+cainang@users.noreply.github.com>
2021-11-30 17:11:10 -0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -12,6 +12,6 @@ app.use(express.json());
 
 app.use(routes);
 
-app.listen(process.env.PORT || 3000, function(){
+app.listen(process.env.PORT || 3251, function(){
   console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
